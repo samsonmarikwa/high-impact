a72b3d416795dbae00379f5cd1a1cc47b00fc152
unknown <samsonmarikwa@outlook.com>
2020-03-08 03:10:11 -0400

    Video and Footer

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,55 +2,63 @@
   <div class="home">
     <div class="card" style="max-width: 100%;">
       <div class="row main-video">
-        <div class="col-md-7">
-          <iframe
-            width="100%"
-            height="450"
-            src="https://www.youtube-nocookie.com/embed/CEC4otBiFyc?rel=0"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-        </div>
-        <div class="col-md-5">
-          <div class="card-body">
-            <h4 class="card-title">Latest Sermon</h4>
-            <h3>Father we love you from the Spirit</h3>
-            <p class="card-text">
-              Brother Jeremiah Davis singing from the Spirit - Father we love
-              you.
-            </p>
-            <p class="card-text">
-              <small class="text-muted">December 7, 2020</small>
-            </p>
+        <div class="col-md-12">
+          <div class="carousel-view">
+            <transition-group class="carousel" tag="div">
+              <div v-for="slide in slides" class="slide" :key="slide.id">
+                <h4>{{ slide.title }}</h4>
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube-nocookie.com/embed/CEC4otBiFyc?rel=0"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </transition-group>
+            <div class="carousel-controls">
+              <button class="carousel-controls__button" @click="previous">
+                prev
+              </button>
+              <button class="carousel-controls__button" @click="next">
+                next
+              </button>
+            </div>
           </div>
-          <hr />
+        </div>
+      </div>
 
+      <div class="row main-video">
+        <div class="col-md-12">
           <div class="card-body">
             <h4 class="card-title">Testimonies</h4>
             <ul style="list-style-type:none">
               <router-link to="/" tag="li">
                 <h5>Testimony One</h5>
                 <p>
-                  Describe situation and how God intervened<span
-                    ><a>Learn more</a></span
-                  >
+                  Describe situation and how God intervened
+                  <span>
+                    <a>Learn more</a>
+                  </span>
                 </p>
               </router-link>
               <router-link to="/" tag="li">
                 <h5>Testimony Two</h5>
                 <p>
-                  Describe situation and how God intervened<span
-                    ><a>Learn more</a></span
-                  >
+                  Now Describe situation and how God intervened
+                  <span>
+                    <a>Learn more</a>
+                  </span>
                 </p>
               </router-link>
               <router-link to="/" tag="li">
                 <h5>Testimony Three</h5>
                 <p>
-                  Describe situation and how God intervened<span
-                    ><a>Learn more</a></span
-                  >
+                  Describe situation and how God intervened
+                  <span>
+                    <a>Learn more</a>
+                  </span>
                 </p>
               </router-link>
               <router-link to="/" tag="li">
@@ -61,18 +69,98 @@
         </div>
       </div>
     </div>
+
+    <div class="footer">
+      <app-footer />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Footer from "./footer/Footer.vue";
+export default {
+  components: {
+    appFooter: Footer
+  },
+  data() {
+    return {
+      slides: [
+        {
+          title: "I am Slide A",
+          id: 1,
+          src: "https://www.youtube-nocookie.com/embed/CEC4otBiFyc?rel=0"
+        },
+        {
+          title: "I am Slide B",
+          id: 2,
+          src: "https://www.youtube-nocookie.com/embed/CEC4otBiFyc?rel=0"
+        },
+        {
+          title: "I am Slide C",
+          id: 3,
+          src: "https://www.youtube-nocookie.com/embed/CEC4otBiFyc?rel=0"
+        },
+        {
+          title: "I am Slide D",
+          id: 4,
+          src: "https://www.youtube-nocookie.com/embed/CEC4otBiFyc?rel=0"
+        },
+        {
+          title: "I am Slide E",
+          id: 5,
+          src: "https://www.youtube-nocookie.com/embed/CEC4otBiFyc?rel=0"
+        }
+      ]
+    };
+  },
+  methods: {
+    next() {
+      const first = this.slides.shift();
+      this.slides = this.slides.concat(first);
+    },
+    previous() {
+      const last = this.slides.pop();
+      this.slides = [last].concat(this.slides);
+    }
+  }
+};
 </script>
-<style scoped>
-.home {
-  border: 1px solid #fff;
-  color: #fff;
-  background-color: #808080;
-  width: 100%;
-}
 
+<style scoped>
+.carousel-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.carousel {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+  width: 100%;
+  /* 24em; */
+  min-height: 35em;
+}
+.slide {
+  flex: 0 0 33%;
+  height: 30em;
+  /* margin: 1em; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0.1em solid #000;
+  /* border-radius: 50%; */
+  transition: transform 0.3s ease-in-out;
+}
+.slide:first-of-type {
+  opacity: 0;
+}
+.slide:last-of-type {
+  opacity: 0;
+}
+.carousel-controls__button {
+  color: #fff;
+  background-color: #555;
+}
 </style>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="footer">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-3">
+        <h4>Info</h4>
+      </div>
+      <div class="col-md-3">
+        <h4>Ministries</h4>
+      </div>
+      <div class="col-md-3">
+        <h4>Visit Us</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <img :src="logoSrc" alt="High Impact Life Church" class="logo" />
+      </div>
+      <div class="col-md-3">
+        <p>
+          <span>
+            <img :src="addressIcon" alt="Address: " class="icons" />
+          </span>Kingstones Building, 3rd Floor
+          <br />
+          <span>
+            <img :src="addressIcon" alt="Address: " class="icons-opacity" />
+          </span>Corner Takawira & Kwame Nkrumah
+          <br />
+          <span>
+            <img :src="addressIcon" alt="Address: " class="icons-opacity" />
+          </span>Harare, Zimbabwe
+        </p>
+        <p>
+          <span>
+            <img :src="clockIcon" alt="Time: " class="icons" />
+          </span>Sunday: 1630 to 1800
+        </p>
+        <p>
+          <span>
+            <img :src="emailIcon" alt="Email: " class="icons" />
+          </span>
+          <a
+            :href="'mailto:info@highimpactlifechurch.org'"
+            target="_top"
+          >info@highimpactlifechurch.org</a>
+        </p>
+        <p>
+          <span>
+            <img :src="telephoneIcon" alt="Call: " class="icons" />
+          </span>+263 77 549 5542
+        </p>
+      </div>
+      <div class="col-md-3">
+        <ul>
+          <router-link to="/" tag="li" active-class="active">
+            <a>High Impact Ladies</a>
+          </router-link>
+          <router-link to="/" tag="li" active-class="active">
+            <a>Men's Fellowship</a>
+          </router-link>
+          <router-link to="/" tag="li" active-class="active">
+            <a>Sunday School</a>
+          </router-link>
+          <router-link to="/" tag="li" active-class="active">
+            <a>Choir</a>
+          </router-link>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <p>Events Calendar</p>
+        <p>Chat online</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12" style="text-align: center">
+        <h6>Copyright &copy;2020 High Impact Life Church</h6>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      logoSrc: "logo.png",
+      addressIcon: "address-icon.png",
+      clockIcon: "clock.png",
+      emailIcon: "email.png",
+      telephoneIcon: "telephone.png"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.footer {
+  color: #636e72;
+  background-color: #a9a9a9;
+  height: 40vh;
+}
+
+.footer a {
+  color: #636e72;
+}
+
+.logo {
+  margin-left: 20%;
+  margin-right: 20%;
+  height: 30vh;
+  max-width: 60%;
+}
+
+.icons,
+.icons-opacity {
+  height: 25px;
+  width: 25px;
+  padding-right: 1px;
+}
+.icons-opacity {
+  opacity: 0;
+}
+
+a {
+  border-bottom: 25px solid transparent;
+}
+
+.col-md-3 ul {
+  list-style-type: none;
+}
+</style>
