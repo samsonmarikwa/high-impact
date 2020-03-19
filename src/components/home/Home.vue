0e9a26f0f697331c712a390dce21908ebc924114
unknown <samsonmarikwa@outlook.com>
2020-03-18 23:42:30 -0400

    Couple and Angels

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <div class="home-background">
-      <img :src="logoSrc" alt="High Impact Life Church" class="logo" />
+      <div class="row">
+        <div class="col-md-9">
+          <img :src="logoSrc" alt="High Impact Life Church" class="logo" />
+        </div>
+        <div class="col-md-3">
+          <img :src="coupleSrc" alt="Prophet Jeremiah and Marble Davis" class="couple" />
+          <br />
+          <img :src="angelsSrc" alt="Activate Angels" class="angels" />
+        </div>
+      </div>
     </div>
     <div class="row">
       <div class="col-md-12">
@@ -19,13 +28,15 @@ import VideoCarousel from "../video-carousel/video-carousel.vue";
 import Footer from "../footer/Footer.vue";
 
 export default {
-    components: {
+  components: {
     VideoCarousel,
     appFooter: Footer
   },
   data() {
     return {
-      logoSrc: "logo.png"
+      logoSrc: "logo.png",
+      coupleSrc: "Couple.jfif",
+      angelsSrc: "angels.jfif"
     };
   }
 };
@@ -35,11 +46,27 @@ export default {
 .home-background {
   text-align: center;
 }
+
 .logo {
-  height: 100%;
-  max-width: 60%;
+  padding: 5vh;
+  height: 80vh;
+  max-width: 90%;
   margin: 0 auto;
-  opacity: 0.4;
+  opacity: 0.6;
+}
+.couple {
+  padding-top: 2vh;
+  height: 30vh;
+  max-width: 80%;
+  margin: 0 auto;
+  opacity: 0.8;
 }
 
+.angels {
+  padding: 10px;
+  height: 60vh;
+  max-width: 100%;
+  margin: 0 auto;
+  opacity: 0.8;
+}
 </style>
